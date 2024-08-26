60dadb63b8aeb359af572c8d79942a6dc8c2a4fb
Anthony Giacalone <anthony.giacalone@csulb.edu>
2024-08-25 17:09:12 -0700

    finalize fall 2024 and update script for future use

--- gen_output.sh
+++ gen_output.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 # Define the output file names
-HTML='cecs-378-fa24-01-syllabus-5505.html'
-PDF='CECS 378_01_FA24_Giacalone_Anthony.pdf'
+COURSE=378
+TYEAR=FA24
+CLASS=01
+SECTION=5505
+NAME=Giacalone_Anthony
 
 # Generate output files
-pandoc README.md -o $HTML --from markdown
-pandoc README.md --template eisvogel -V linkcolor=blue -V header-includes:'\usepackage[export]{adjustbox} \let\includegraphicsbak\includegraphics \renewcommand*{\includegraphics}[2][]{\includegraphicsbak[frame,#1]{#2}}' -o $PDF
+pandoc README.md -o cecs-$COURSE-$TYEAR-$CLASS-syllabus-$SECTION.html --from markdown
+pandoc README.md --template eisvogel -V linkcolor=blue -V header-includes:'\usepackage[export]{adjustbox} \let\includegraphicsbak\includegraphics \renewcommand*{\includegraphics}[2][]{\includegraphicsbak[frame,#1]{#2}}' -o CECS\ $COURSE\_$CLASS\_$TYEAR\_$NAME.pdf
